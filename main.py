e911e383a8c5ccfdff96c5afe0722a252808e044
Gal Bachar <gal.bachar@cymotive.com>
2022-05-02 16:43:23 +0300

    normalize data

--- main.py
+++ main.py
@@ -5,15 +5,17 @@ import pandas as pd
 import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def test():
-    driver = webdriver.Chrome('C:/Users/yuval/PycharmProjects/Data-science-Weather/chromedriver/chromedriver')
+def data_collect():
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome('/Users/gal.bachar/git_wa/HIT/Data-science-Weather/chromedriver/chromedriver',
+                              options=chrome_options)
     df = pd.DataFrame([], columns=["year", "month", "day", "temp", "humidity", "windspeed", "precipitation"])
     df_row_count=1
 
@@ -22,30 +24,56 @@ def test():
     settings = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "wuSettings")))
     settings.click()
     time.sleep(2)
-    celsius = driver.find_element(By.XPATH, '//*[@id="wuSettings-quick"]/div/a[2]').click()
+    celsius = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH,
+                                                                              '//*[@id="wuSettings-quick"]/div/a[2]')))
+    celsius.click()
 
-    for year in range(2000,2022):
+    for year in range(2000,2023):
         for month in range(1,13):
             driver.get(f"https://www.wunderground.com/history/monthly/us/ny/new-york-city/KLGA/date/{year}-{month}")
-            table_id= WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "days")))
-            data = table_id.find_elements(By.TAG_NAME, "table") # get all of the rows in the table
-            num_of_days = len(data[1].find_elements(By.TAG_NAME, "tr"))
+            try:
+                table_id= WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "days")))
+                data = table_id.find_elements(By.TAG_NAME, "table") # get all of the rows in the table
+                num_of_days = len(data[1].find_elements(By.TAG_NAME, "tr"))
 
-            for i in range(1,num_of_days):
-                day = data[0].find_elements(By.TAG_NAME, "tr")[i].text
-                temp = data[1].find_elements(By.TAG_NAME, "tr")[i].text.split(' ')[1]
-                humidity = data[3].find_elements(By.TAG_NAME, "tr")[i].text.split(' ')[1]
-                windspeed = data[4].find_elements(By.TAG_NAME, "tr")[i].text.split(' ')[1]
-                precipitation = data[6].find_elements(By.TAG_NAME, "tr")[i].text
-                day_data = [year,month,day,temp,humidity,windspeed,precipitation]
-                df.loc[df_row_count] = day_data
-                df_row_count+=1
+                for i in range(1,num_of_days):
+                    day = int(data[0].find_elements(By.TAG_NAME, "tr")[i].text)
+                    temp = float(data[1].find_elements(By.TAG_NAME, "tr")[i].text.split(' ')[1])
+                    humidity = float(data[3].find_elements(By.TAG_NAME, "tr")[i].text.split(' ')[1])
+                    windspeed = float(data[4].find_elements(By.TAG_NAME, "tr")[i].text.split(' ')[1])
+                    precipitation = float(data[6].find_elements(By.TAG_NAME, "tr")[i].text)
+                    day_data = [year,month,day,temp,humidity,windspeed,precipitation]
+                    df.loc[df_row_count] = day_data
+                    df_row_count += 1
+            except Exception as e:
+                print(f"cloud not get {year}-{month} data: {e}")
+
     driver.quit()
-    print(df)
+    #df.to_csv('~/Documents/nyc_weather.csv',index=False) #for debugging
+    return df
 
 
-# Press the green button in the gutter to run the script.
+def normalize_data(data_df, good_weather_values):
+    norm_df = data_df.copy()
+    for i in norm_df.columns:
+        if i not in ['year', 'month', 'day']:
+            good_min = good_weather_values[i][0]
+            good_max = good_weather_values[i][1]
+            diff_min = good_weather_values[i][0] - norm_df[i].min()
+            diff_max = norm_df[i].max() - good_weather_values[i][1]
+            norm_df.loc[(norm_df[i] < good_min), i] = 1 - ((good_min - norm_df[i]) / diff_min)
+            norm_df.loc[(norm_df[i] > good_max), i] = 1 - ((norm_df[i] - good_max) / diff_max)
+            norm_df.loc[(norm_df[i] <= good_max) & (norm_df[i] >= good_min), i] = 1
+    return norm_df
+
+
 if __name__ == '__main__':
-    test()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # nice weather:
+    # temp 18-25
+    # humidity 30-50%
+    # windspeed 0-7
+    # precipitation 0 - 0.5
+    good_weather_values = {'temp': [18,25], 'humidity':[40,50],'windspeed':[4,10], 'precipitation':[0,0.5]}
+    df = data_collect()
+    # df = pd.read_csv('~/Documents/nyc_weather.csv') # for debugging
+    norm_df = normalize_data(df, good_weather_values)
